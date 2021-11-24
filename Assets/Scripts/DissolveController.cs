d0464e2b937dbcd8aed9376c5b7ce15ad600283a
Gitti Ekchan <gittitat@mohara.co>
2021-11-24 13:30:40 +0700

    [ADD] Particle System to AR effect

--- Assets/Scripts/DissolveController.cs
+++ Assets/Scripts/DissolveController.cs
@@ -11,6 +11,13 @@ public class DissolveController : MonoBehaviour
     private float initialDissolve = -1.5f;
     [SerializeField]
     private float targetDissolve = 2f;
+    [SerializeField]
+    private ParticleSystem spawnEffect;
+    [SerializeField]
+    private float  particleStartLifeTime;
+    [SerializeField]
+    private float particleEndLifeTime;
+
 
     private string dissolveKey = "_DissolveValue";
     void Start()
@@ -30,6 +37,17 @@ public class DissolveController : MonoBehaviour
             dissolveVal += Time.deltaTime;
             yield return endOfFrame;
             dissolveMaterials.ForEach(mat => mat.SetFloat(dissolveKey, dissolveVal));
+            var mainModule = spawnEffect.main;
+            var lifeTime = mainModule.startLifetime;
+            lifeTime.constant = Remap(initialDissolve,targetDissolve, particleStartLifeTime, particleEndLifeTime, dissolveVal);
+            mainModule.startLifetime = lifeTime;
         }
+        spawnEffect.gameObject.SetActive(false);
+    }
+
+    private float Remap(float inMin,float inMax, float outMin,float outMax,float val)
+    {
+        var t = Mathf.InverseLerp(inMin,inMax,val);
+           return Mathf.Lerp(outMin,outMax,t);
     }
 }
